ae9703ea0afc6a33f2c62567087846fd4efb8441
Mike <394438830@qq.com>
2023-04-30 18:38:46 +0100

    doing teacher statistic

--- aat/routes.py
+++ aat/routes.py
@@ -609,6 +609,7 @@ def statistic(assessment_id):
 def assessments():
    return render_template('assessments.html')
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
