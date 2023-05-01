b7fd05555911fd4a615d8aec7633503f24ae42f1
andy0427s <andy0427s@gmail.com>
2023-05-01 08:19:57 +0100

    Fix edit funt for MC

--- aat/routes.py
+++ aat/routes.py
@@ -8,6 +8,7 @@ from io import TextIOWrapper, StringIO
 from sqlalchemy import func
 from urllib.parse import unquote
 
+
 import random, csv, io
 
 
@@ -171,18 +172,27 @@ def delete_st_question(question_id):
 
 @app.route('/edit-mc-question/<int:question_id>', methods=['GET', 'POST'])
 def edit_mc_question(question_id):
+    # Get the question from the database
     question = McQuestion.query.get_or_404(question_id)
+    print(question_id)
+    # Create an instance of the form and fill it with the current question data
     form = McQuestionForm(obj=question)
-    if form.validate_on_submit():
+    print(question_id)
+    if request.method == "POST":
+        # Update the question object with the form data
+        print(question_id)
         form.populate_obj(question)
-        question.correct_choice_id = MC_CHAR_ID[form.correct_choice.data]
+        # Save the changes to the database
         db.session.commit()
         flash('Question updated successfully!', category='success')
+        # Redirect to the questions list page
         return redirect(url_for('questions'))
     return render_template('edit_mc_question.html', form=form, question=question)
 
+
 @app.route('/edit-st-question/<int:question_id>', methods=['GET', 'POST'])
 def edit_st_question(question_id):
+    print(question_id)
     question = StQuestion.query.get_or_404(question_id)
     form = StQuestionForm(obj=question)
     if form.validate_on_submit():
